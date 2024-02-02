c18979c043aea3d3b3228cde8cd819cafa40edf3
DanteCastilleja <dsc2386@utulsa.edu>
2024-02-02 15:49:35 -0600

    Problem 2

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,6 +1,10 @@
 def telephone():
     print('Phone Number: ')
     phone_number = int(input())
+    area_code = phone_number // 10000000
+    first3 = (phone_number % 10000000) // 10000
+    last4 = phone_number % 10000
+    print(f'({area_code}){first3}-{last4}')
     ''' Type your code here. '''
 if __name__ == "__main__":
     telephone()
