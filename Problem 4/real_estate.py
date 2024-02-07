b57cc7099b5714e23a0e440e4bb2351317213a01
DanteCastilleja <dsc2386@utulsa.edu>
2024-02-07 14:49:09 -0600

    removed spaces

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -6,7 +6,7 @@ def real_estate():
     print('Last month price = ')
     last_month_price = int(input())
     mortgage = (current_price * 0.051) / 12
-    print(f'This house is ${current_price}. The change is ${current_price - last_month_price:} since last month. \nThe estimated monthly mortgage is ${mortgage :.2f}.')
+    print(f'This house is ${current_price}. The change is ${current_price - last_month_price} since last month. \nThe estimated monthly mortgage is ${mortgage:.2f}.')
 
 if __name__ == "__main__":
     real_estate()
