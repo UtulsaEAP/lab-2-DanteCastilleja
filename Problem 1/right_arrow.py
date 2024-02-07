dae2b5069d83c998f250e7765dd864c84aec41d5
DanteCastilleja <dsc2386@utulsa.edu>
2024-02-07 15:06:53 -0600

    spaces

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -4,7 +4,7 @@ def right_arrow():
     print("head_char = ")
     head_char = input()
 
-    row1 = base_char * 6 + head_char
+    row1 = '      ' + head_char
     row2 = base_char * 6 + head_char * 2
     row3 = base_char * 6 + head_char * 3
     print(row1)
